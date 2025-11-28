6cb6a90aeb8e12a8d7955edf4836ed9319e8b86e
mahdiarghobadi6536-cloud <mahdiar.ghobadi6536@gmail.com>
2025-11-28 18:53:39 +0330

    رفع ارور 2

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 from telegram.ext import Application, CommandHandler
+
 TOKEN = "8577813513:AAGQVgFm45nBLog6ZIK6Ui7fU8dqCiwGpxE"
 
-
 async def start(update, context):
-    await update.message.reply_text("سلام ! ربات روشن شد ")
+    await update.message.reply_text("سلام! ربات روشن شد.")
+
 async def help_command(update, context):
     await update.message.reply_text(
         "راهنما:\n"
-        "/start - شروع ربات \n"
-        "help - راهنمای دستورات \n"
-        "هرچی بنویسی تکرار میکنم !"
-    ) 
-App= Application.builder().token(TOKEN).build()
+        "/start - شروع ربات\n"
+        "/help - راهنمای دستورات"
+    )
 
-App.add_handler(CommandHandler("start" , start))
-App.add_handler(CommandHandler("help",help_command))
+def main():
+    app = Application.builder().token(TOKEN).build()
 
-App.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
